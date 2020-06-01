327cc602e40ba8e23f2a8fb8fecd8f03ca386af6
Lucas Cavalcanti <lucas@mercadons.com.br>
2020-06-01 01:05:58 -0300

    init

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import api from '../../services/api';
+
+import './style.css';
+
+
+const Dashboard: React.FC = () => {
+  return (
+    <h1>React Template</h1>
+  );
+};
+
+export default Dashboard;
